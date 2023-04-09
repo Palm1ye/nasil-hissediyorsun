f4d393fd6706fd509881b2a8d7ca6eb95a38b501
Palm1ye <ardatasofficial@gmail.com>
2023-04-10 00:55:31 +0300

    Database connection

--- script.js
+++ script.js
@@ -16,31 +16,64 @@ form.addEventListener('submit', (e) => {
         return;
     }
 
-    const emotionItem = document.createElement('li');
-    const emotionDetails = document.createElement('p');
-    const emotionMeta = document.createElement('div');
-    const emotionTime = document.createElement('span');
+    const data = {
+        name: name,
+        emotion: emotion,
+        details: details,
+        timestamp: new Date().getTime()
+    };
 
-    emotionDetails.textContent = name + ": " + details;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/emotions', true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const emotionItem = document.createElement('li');
+            const emotionDetails = document.createElement('p');
+            const emotionMeta = document.createElement('div');
+            const emotionTime = document.createElement('span');
 
-    if (name) {
-        const emotionName = document.createElement('span');
-        emotionName.textContent = 'Duygu ' + ': ' + emotion;
-        emotionMeta.appendChild(emotionName);
-    }
+            emotionDetails.textContent = data.name + ': ' + data.details;
 
+            if (data.name) {
+                const emotionName = document.createElement('span');
+                emotionName.textContent = 'Duygu ' + ': ' + data.emotion;
+                emotionMeta.appendChild(emotionName);
+            }
 
-
-    emotionTime.textContent = new Date().toLocaleString();
-
-    emotionMeta.appendChild(emotionTime);
-
-    emotionItem.appendChild(emotionDetails);
-    emotionItem.appendChild(emotionMeta);
-
-    emotionsList.insertBefore(emotionItem, emotionsList.firstChild);
-
-    nameInput.value = '';
-    emotionInput.value = '';
-    detailsInput.value = '';
+            emotionTime.textContent = new Date(data.timestamp).toLocaleString();
+            emotionMeta.appendChild(emotionTime);
+            emotionItem.appendChild(emotionDetails);
+            emotionItem.appendChild(emotionMeta);
+            emotionsList.insertBefore(emotionItem, emotionsList.firstChild);
+            nameInput.value = '';
+            emotionInput.value = '';
+            detailsInput.value = '';
+        }
+    };
+    xhr.send(JSON.stringify(data));
 });
+
+window.onload = async () => {
+    const data = await fetch("/emotions").then(res => res.json());
+    for (const item of data) {
+        const emotionItem = document.createElement('li');
+        const emotionDetails = document.createElement('p');
+        const emotionMeta = document.createElement('div');
+        const emotionTime = document.createElement('span');
+
+        emotionDetails.textContent = item.name + ': ' + item.details;
+
+        if (item.name) {
+            const emotionName = document.createElement('span');
+            emotionName.textContent = 'Duygu ' + ': ' + item.emotion;
+            emotionMeta.appendChild(emotionName);
+        }
+
+        emotionTime.textContent = new Date(item.timestamp).toLocaleString();
+        emotionMeta.appendChild(emotionTime);
+        emotionItem.appendChild(emotionDetails);
+        emotionItem.appendChild(emotionMeta);
+        emotionsList.insertBefore(emotionItem, emotionsList.firstChild);
+    }
+}    
